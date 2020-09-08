452f8e2d19d325ea0f4b6a8b58cf56e2585f6612
unknown <narinzeanthony@yahoo.com>
2020-09-08 12:56:06 +0100

    initial commit of facerocognition app

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Rank() {
+    return (
+        <div>
+            <div className='center white f3'>
+                {'Andrei, your current rank is...'}
+            </div>
+            <div className='center white f1'>
+                {'#3'}
+            </div>
+        </div>
+    )
+}
+export default Rank;
